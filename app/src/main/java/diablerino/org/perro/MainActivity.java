b1792497e6f94a4b9104a80c3811642f1d6ec136
io <luis.herrerae@outlook.com>
2018-02-09 17:46:08 -0600

    Servicios y activities

--- app/src/main/java/diablerino/org/perro/MainActivity.java
+++ app/src/main/java/diablerino/org/perro/MainActivity.java
@@ -1,24 +1,28 @@
 package diablerino.org.perro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText textoCuerpo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.enviar).setOnClickListener(new View.OnClickListener() { //Evento -> Clase interna anónima
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Tumensaje se envió",Toast.LENGTH_LONG).show();
 
+        textoCuerpo = (EditText) findViewById(R.id.mensaje);
+        findViewById(R.id.compartir).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {                       //Intent: Clase externa
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND); //Busqueda de aplicaciones que pueden enviar el mensaje
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, textoCuerpo.getText().toString());
+                startActivity(Intent.createChooser(sharingIntent, "Enviando Mensaje"));  //Escoger la aplicación donde se va a compartir
             }
-        }); //Carpeta R. =res
+        });
     }
 }
